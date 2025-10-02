3d76e60dfa7a6a932a29ef1c3485488b2814035f
Mouzam ali <malikmouzam316@gmail.com>
2025-10-02 11:08:27 +0500

    cors change

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/auth.js";
 import notesRoutes from "./routes/notes.js";
 import path from "path";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-
 const app = express();
 
+// Middleware
+app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/users", authRoutes);
 app.use("/api/notes", notesRoutes);
 
 const __dirname = path.resolve();
 
+// Serve frontend in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("/{*splat}", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
+// Database connection
 connectDB();
 
 app.listen(PORT, () => {
